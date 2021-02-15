ede8afb04cc45b7dcec42fcbf366a58050a38e25
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2021-02-15 17:28:42 +0100

    Hello World Mini-Task

--- packages/playground/src/samples/simple.js
+++ packages/playground/src/samples/simple.js
@@ -3,72 +3,31 @@ module.exports = {
     title: "A registration form",
     description: "A simple form example.",
     type: "object",
-    required: ["firstName", "lastName"],
+    required: ["email", "password"],
     properties: {
-      firstName: {
+      email: {
         type: "string",
-        title: "First name",
-        default: "Chuck",
+        title: "E-Mail",
       },
-      lastName: {
+      password: {
         type: "string",
-        title: "Last name",
-      },
-      // age: {
-      //   type: "integer",
-      //   title: "Age",
-      // },
-      // bio: {
-      //   type: "string",
-      //   title: "Bio",
-      // },
-      // password: {
-      //   type: "string",
-      //   title: "Password",
-      //   minLength: 3,
-      // },
-      telephone: {
-        type: "string",
-        title: "Telephone",
-        minLength: 10,
+        title: "Password",
+        minLength: 8,
       },
     },
   },
   uiSchema: {
-    firstName: {
+    email: {
       "ui:autofocus": true,
       "ui:emptyValue": "",
-      "ui:autocomplete": "family-name",
-    },
-    lastName: {
-      "ui:emptyValue": "",
-      "ui:autocomplete": "given-name",
-    },
-    age: {
-      "ui:widget": "updown",
-      "ui:title": "Age of person",
-      "ui:description": "(earthian year)",
-    },
-    bio: {
-      "ui:widget": "textarea",
     },
     password: {
       "ui:widget": "password",
       "ui:help": "Hint: Make it strong!",
     },
-    date: {
-      "ui:widget": "alt-datetime",
-    },
-    telephone: {
-      "ui:options": {
-        inputType: "tel",
-      },
-    },
   },
   formData: {
-    lastName: "Norris",
-    age: 75,
-    bio: "Roundhouse kicking asses since 1940",
-    password: "noneed",
+    email: "",
+    password: "",
   },
 };
